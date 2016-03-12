b3af9a641be7ccc33ad5b07538a738be0046def2
Noah Smith <noahhsmith@gmail.com>
2016-03-12 18:18:04 +0100

    test

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    vec a = { 1 , 2 };
+    cout << a << endl;
+
+    return 0;
+}
+
