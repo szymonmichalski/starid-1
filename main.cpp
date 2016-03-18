370035024ba92237540673d815af297a96d7d8b9
Noah Smith <noahhsmith@gmail.com>
2016-03-18 12:54:53 +0100

    Initial read from skymap catalog.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
-#include <iostream>
 #include <armadillo>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
     using namespace std;
     using namespace arma;
 
-    cout << "Hello World!" << endl;
+    string line;
+    ifstream catfile ("../SKYMAP_SKY2000_V5R4.txt");
+    if (catfile.is_open()) {
+        getline(catfile,line);
+        cout << line << '\n';
+        catfile.close();
+    } else {
+        cout << "Catalog file not found." << "\n";
+    }
 
     mat a = { {1,0} , {0,1} };
     cout << inv(a) << endl;
