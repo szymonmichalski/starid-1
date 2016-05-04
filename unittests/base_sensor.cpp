d9145704239be2c8de501082d5f08e3066828b76
Noah Smith <noahhsmith@gmail.com>
2016-05-04 13:27:08 +0200

    bug fix

--- unittests/base_sensor.cpp
+++ unittests/base_sensor.cpp
@@ -2,32 +2,22 @@
 #include "catalog.h"
 #include "sensor.h"
 
-#include <chrono>
-//#include <iostream>
-//#include <string>
 #include <armadillo>
 #include "gtest/gtest.h"
 
-double UnixTimeToJ2000Offset = 946684800.0;
-double RaOrion = 75.0 * arma::datum::pi / 180.0;
-double DecOrion = 0.0 * arma::datum::pi / 180.0;
-double RaCass = 0.0 * arma::datum::pi / 180.0;
-double DecCass = 60.0 * arma::datum::pi / 180.0;
-
 TEST(base_sensor, endtoend)
 {
-    std::chrono::time_point<std::chrono::system_clock> tcurrent {std::chrono::system_clock::now()};
-    double years_from_j2000 {(double(std::chrono::system_clock::to_time_t(tcurrent)) - UnixTimeToJ2000Offset) / 31557600.0}; // julian years
-    double fovradius {4.0 * arma::datum::pi / 180.0};
-    double mv {6.5};
+    std::string fcatalog = "../../SKYMAP_SKY2000_V5R4.txt";
+    double t = 0.0;
+    double mv = 6.5;
+    double fov = 4.0 * arma::datum::pi / 180.0;
+    double ra = 0.0 * arma::datum::pi / 180.0;
+    double dec = 60.0 * arma::datum::pi / 180.0;
+    double yaw = 0.0 * arma::datum::pi / 180.0;
+    base::Pointing pointing(ra, dec, yaw);
 
-    std::string catalog_file {"../../SKYMAP_SKY2000_V5R4.txt"};
-    base::Catalog catalog(catalog_file, years_from_j2000, mv);
-
-    double yaw {0.0 * arma::datum::pi / 180.0};
-    base::Pointing pointing(RaCass, DecCass, yaw);
-    base::Sensor sensor(fovradius, mv);
-//    sensor.SetPointing(pointing);
-//    base::L1 obs = sensor.Level1(catalog);
-//    EXPECT_EQ(obs.tpc.n_rows, (unsigned)23);
+    base::Catalog catalog(fcatalog, t, mv);
+    base::Sensor sensor(fov, mv);
+    sensor.Update(catalog, pointing);
+    EXPECT_EQ(sensor.l1.tpc.n_rows, (unsigned)23);
 }
