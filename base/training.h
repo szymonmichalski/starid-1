aad36136daa9a296b5ef4ae7153f6067034dd210
Noah Smith <noahhsmith@gmail.com>
2016-05-20 14:34:38 +0200

    trainingset struct

--- base/training.h
+++ base/training.h
@@ -5,6 +5,11 @@
 
 namespace training {
 
+struct TrainingSet {
+    arma::mat examples; // n x l matrix of n dimensional feature vectors
+    arma::vec labels; // l x 1 vector of class labels -1 or 1
+};
+
 class Training {
 public:
 private:
