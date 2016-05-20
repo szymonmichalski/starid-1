5ff34f9961b3be9fe8103ca504ae534aa76cfaac
Noah Smith <noahhsmith@gmail.com>
2016-05-20 15:45:26 +0200

    update training

--- base/training.cpp
+++ base/training.cpp
@@ -1,3 +1,11 @@
 #include "training.h"
 
+base::Training::Training(uint cnt_inclass, uint cnt_outofclass) {
+    TrainingSet trainingset;
+    trainingset.cntinclass = cnt_inclass;
+    trainingset.cntoutofclass = cnt_outofclass;
+}
 
+void base::Training::Status() {
+
+}
--- base/training.h
+++ base/training.h
@@ -3,15 +3,19 @@
 
 #include <armadillo>
 
-namespace training {
+namespace base {
 
 struct TrainingSet {
     arma::mat examples; // n x l matrix of n dimensional feature vectors
-    arma::vec labels; // l x 1 vector of class labels -1 or 1
+    arma::vec labels; // l x 1 vector of class labels -1 (outofclass) or 1 (inclass)
+    uint cntinclass;
+    uint cntoutofclass;
 };
 
 class Training {
 public:
+    Training(uint cnt_inclass, uint cnt_outofclass);
+    void Status();
 private:
 };
 
