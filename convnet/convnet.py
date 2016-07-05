d268e404773c46e2c60650b587337dbe88776282
Noah Smith <noahhsmith@gmail.com>
2016-07-05 08:32:20 +0200

    exp

--- convnet/convnet.py
+++ convnet/convnet.py
@@ -5,7 +5,7 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('/tmp/data', one_hot=True)
+input = input_data.read_data_sets('/tmp/data', one_hot=True)
 sess = tf.InteractiveSession()
 
 x = tf.placeholder(tf.float32, shape=[None, 784])
@@ -28,19 +28,19 @@ def conv2d(x, W):
 def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-with tf.name_scope('convolution1'):
+with tf.name_scope('conv1'):
   W_conv1 = weight_variable([5, 5, 1, 32])
   b_conv1 = bias_variable([32])
   h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
   h_pool1 = max_pool_2x2(h_conv1)
 
-with tf.name_scope('convolution2'):
+with tf.name_scope('conv2'):
   W_conv2 = weight_variable([5, 5, 32, 64])
   b_conv2 = bias_variable([64])
   h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
   h_pool2 = max_pool_2x2(h_conv2)
 
-with tf.name_scope('fully_connected'):
+with tf.name_scope('local1'):
   W_fc1 = weight_variable([7 * 7 * 64, 1024])
   b_fc1 = bias_variable([1024])
   h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
@@ -48,19 +48,19 @@ with tf.name_scope('fully_connected'):
   keep_prob = tf.placeholder(tf.float32)
   h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-with tf.name_scope('output'):
+with tf.name_scope('classification'):
   W_fc2 = weight_variable([1024, 10])
   b_fc2 = bias_variable([10])
   y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-with tf.name_scope('cross_entropy'):
-  cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-  tf.scalar_summary('cross entropy', cross_entropy)
+with tf.name_scope('loss'):
+  loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+  tf.scalar_summary('loss', loss)
 
 with tf.name_scope('train'):
-  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
-with tf.name_scope('accuracy'):
+with tf.name_scope('evaluate'):
   with tf.name_scope('correct_prediction'):
     correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
   with tf.name_scope('accuracy'):
@@ -72,12 +72,12 @@ train_writer = tf.train.SummaryWriter('/tmp/convnet_logs' + '/train', sess.graph
 sess.run(tf.initialize_all_variables())
 
 for i in range(1000):
-  batch = mnist.train.next_batch(50)
+  batch = input.train.next_batch(50)
   if i%100 == 0:
     train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g"%(i, train_accuracy))
   summary, _ = sess.run([merged, train_step], feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
   train_writer.add_summary(summary, i)
 
-print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+print("test accuracy %g"%accuracy.eval(feed_dict={x: input.test.images, y_: input.test.labels, keep_prob: 1.0}))
 
