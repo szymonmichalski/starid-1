678dfc0be52e8ed8074c3dee5e490250add1c4b4
Noah Smith <noahhsmith@gmail.com>
2016-07-09 14:47:10 +0200

    training is working

--- convnet/convnet_train.py
+++ convnet/convnet_train.py
@@ -2,9 +2,22 @@
 import numpy as np
 import tensorflow as tf
 import convnet
+import convnet_input
 
 def train():
-    inputs = convnet.inputs()
+
+    images, labels = convnet_input.training_inputs()
+    logits = convnet.inference(images)
+    loss = convnet.loss(logits, labels)
+    train_op = convnet.train(loss)
+
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+    for i in range(10):
+        loss_value = sess.run(loss)
+        print("step %d, loss %g" % (i, loss_value))
+        sess.run(train_op)
 
 def main(argv=None):
     train()
