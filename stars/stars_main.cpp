c1e4f973fc0c86befc98aa3eb8ed4b9f93ab0c3b
Noah Smith <noahhsmith@gmail.com>
2016-07-28 13:19:50 +0200

    basic starid_data complete

--- stars/stars_main.cpp
+++ stars/stars_main.cpp
@@ -7,12 +7,12 @@ int main() {
     bool mnist_to_starid  = true;
     bool yaw              = true;
     int number_of_images  = 10000;
-    std::string f_img1    = "/home/noah/dev/starid_data/mnist_format/mnist2_images.mnist";
-    std::string f_lab1    = "/home/noah/dev/starid_data/mnist_format/mnist2_labels.mnist";
-    std::string f_img2    = "/home/noah/dev/starid_data/mnist_format/starid2_yawed_images.mnist";
-    std::string f_lab2    = "/home/noah/dev/starid_data/mnist_format/starid2_yawed_labels.mnist";
+    std::string f_img1    = "/home/noah/dev/starid_data/mnist_format/mnist_imagesb.mnist";
+    std::string f_lab1    = "/home/noah/dev/starid_data/mnist_format/mnist_labelsb.mnist";
+    std::string f_img2    = "/home/noah/dev/starid_data/mnist_format/starid_imagesb_yawed.mnist";
+    std::string f_lab2    = "/home/noah/dev/starid_data/mnist_format/starid_labelsb_yawed.mnist";
 
-    std::string f_catalog = "/home/noah/dev/starid_input_data/SKYMAP_SKY2000_V5R4.txt";
+    std::string f_catalog = "/home/noah/dev/starid_data/SKYMAP_SKY2000_V5R4.txt";
     double mv             = 6.5;
     double fov            = 4.0 * arma::datum::pi / 180.0;
 
@@ -24,13 +24,13 @@ int main() {
 
     if (mnist_to_starid) {
         stars::Sensor sensor(f_catalog, mv, fov);
-        for (uint itrcnt = 0; itrcnt < 100; ++itrcnt) {
+        for (uint itrcnt = 0; itrcnt < number_of_images/10; ++itrcnt) {
             for (uint label = 0; label < 10; ++label) {
                 uint starndx = 800 * (label + 1);
                 arma::mat img = sensor.Image(starndx);
                 labels(10*itrcnt + label) = (double)label;
                 images[10*itrcnt + label] = img;
-                //sensor.Status();
+                sensor.Status();
                 int a = 1;
             }
         }
