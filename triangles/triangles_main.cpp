618d7934af34304b6fe8ed918c1abf90e56063c8
Noah Smith <noahhsmith@gmail.com>
2016-11-21 07:43:52 +0100

    backup

--- triangles/triangles_main.cpp
+++ triangles/triangles_main.cpp
@@ -5,19 +5,19 @@
 int main() {
     arma::arma_rng::set_seed_random();
 
-    std::string f_catalog = "/home/noah/dev/SKYMAP_SKY2000_V5R4.txt";
-    double mv             = 6.5;
-    double fov            = 4.0 * arma::datum::pi / 180.0;
-    uint starndx          = 800;
-    double triplets_tol   = 60 * arma::datum::pi / 648e3;
-    uint triplets_max     = 1e3;
+    std::string fcatalog   = "/home/noah/dev/SKYMAP_SKY2000_V5R4.txt";
+    double mv              = 6.5; // sensor upper limit for visual magnitude
+    double fov             = 4.0 * arma::datum::pi / 180.0; // sensor field of view
+    uint starndx           = 800; // star to be used
+    double triangles_tol   = 60 * arma::datum::pi / 648e3; // tolerance for triangle variations
+    uint triangles_max     = 1e3; // upper limit for number of triangles
 
-    stars::Sensor sensor(f_catalog, mv, fov);
-
-    triangles::TrianglesInStarImage triangles(sensor, triplets_tol, triplets_max);
-
+    // create a sensor and make a star image
+    stars::Sensor sensor(fcatalog, mv, fov);
     sensor.MakeStarImage(starndx);
 
+    // create star triangles in the image and try to recognize a triangle
+    triangles::TrianglesInStarImage triangles(sensor, triangles_tol, triangles_max);
     triangles.RecognizeTriangleInStarImage();
 
     return 0;
