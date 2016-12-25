8cdf880ccdb743c24425749d8df4f7312fad8b13
Noah Smith <noahhsmith@gmail.com>
2016-12-25 11:31:53 +0100

    sync

---
+++ heuristics/triangles_in_star_image.cpp
@@ -0,0 +1,84 @@
+#include "triangles_in_star_image.h"
+
+triangles::TrianglesInStarImage::TrianglesInStarImage(stars::Sensor &sensor, double triangle_tol, uint max_triangles) :
+    sensor(sensor),
+    triangle_tol(triangle_tol),
+    max_triangles(max_triangles),
+    num_stars(sensor.l1_uvec.n_rows),
+    cur_triangle(0)
+{
+    pairsOverWholeSky.init(sensor);
+}
+
+void triangles::TrianglesInStarImage::recognizeTriangleInStarImage() {
+    arma::mat mata;
+    arma::mat matb;
+    arma::mat matc;
+    mata.zeros(max_triangles,3);
+    matb.zeros(max_triangles,3);
+    matc.zeros(max_triangles,3);
+
+    cur_triangle = 0;
+    uint i, j, k, dj, dk;
+    for (dj = 1; dj <= num_stars-2; ++dj) {
+        for (dk = 1; dk <= num_stars-dj-1; ++dk) {
+            for (i = 1; i <= num_stars-dj-dk; ++i) {
+                j = i + dj;
+                k = j + dk;
+                mata.row(cur_triangle) = sensor.l1_uvec.row(i-1);
+                matb.row(cur_triangle) = sensor.l1_uvec.row(j-1);
+                matc.row(cur_triangle) = sensor.l1_uvec.row(k-1);
+                ++cur_triangle;
+                if (cur_triangle == max_triangles-1) break;
+            }
+            if (cur_triangle == max_triangles-1) break;
+        }
+        if (cur_triangle == max_triangles-1) break;
+    }
+
+    if (cur_triangle < max_triangles-1) {
+        mata.shed_rows(cur_triangle,max_triangles-1);
+        matb.shed_rows(cur_triangle,max_triangles-1);
+        matc.shed_rows(cur_triangle,max_triangles-1);
+    }
+
+    for (uint i = 0; i < mata.n_rows; ++i) {
+
+        triangles::Triangle triangle;
+
+        triangle.uva = arma::trans(mata.row(i));
+        triangle.uvb = arma::trans(matb.row(i));
+        triangle.uvc = arma::trans(matc.row(i));
+
+        triangle.angab = acos(arma::dot(triangle.uva, triangle.uvb));
+        triangle.angac = acos(arma::dot(triangle.uva, triangle.uvc));
+        triangle.angbc = acos(arma::dot(triangle.uvb, triangle.uvc));
+
+        // triangle sides are too similar
+        if (std::abs(triangle.angab - triangle.angac) < triangle_tol
+            || std::abs(triangle.angab - triangle.angbc) < triangle_tol
+            || std::abs(triangle.angac - triangle.angbc) < triangle_tol) continue;
+
+        std::vector<int> l1ab = pairsOverWholeSky.starsFromPairs(triangle.angab, 0.05 * triangle_tol);
+        std::vector<int> l1ac = pairsOverWholeSky.starsFromPairs(triangle.angac, 0.05 * triangle_tol);
+        std::vector<int> l1bc = pairsOverWholeSky.starsFromPairs(triangle.angbc, 0.05 * triangle_tol);
+
+        std::vector<int> l2abac;
+        std::vector<int> l2babc;
+        std::vector<int> l2cacb;
+        std::set_intersection(l1ab.begin(), l1ab.end(),
+                              l1ac.begin(), l1ac.end(),
+                              back_inserter(l2abac));
+        std::set_intersection(l1ab.begin(), l1ab.end(),
+                              l1bc.begin(), l1bc.end(),
+                              back_inserter(l2babc));
+        std::set_intersection(l1ac.begin(), l1ac.end(),
+                              l1bc.begin(), l1bc.end(),
+                              back_inserter(l2cacb));
+        continue;
+    }
+
+    std::cout << std::endl;
+}
+
+
--- stars/stars_main.cpp
+++ stars/stars_main.cpp
@@ -27,7 +27,7 @@ int main() {
     }
 
     if (0) { // generate star images in mnist format
-        int number_of_images  = 10000;
+        int number_of_images  = 60000; // 60k for training file, 10k for test file
         std::string fimg1    = "/home/noah/dev/starid/data/mnist_format/mnist_imagesb.mnist";
         std::string flab1    = "/home/noah/dev/starid/data/mnist_format/mnist_labelsb.mnist";
         std::string fimg2    = "/home/noah/dev/starid/data/mnist_format/starid_imagesb.mnist";
@@ -40,7 +40,7 @@ int main() {
         stars::Sensor sensor(f10stars, mv, fov);
         for (int tenscnt = 0; tenscnt < number_of_images/10; ++tenscnt) {
             for (int label = 0; label < 10; ++label) {
-                int starndx = label;
+                int starndx = 800 * (label+1);
                 arma::mat img = sensor.makeStarImage(starndx);
                 labels(10*tenscnt + label) = (double)label;
                 images[10*tenscnt + label] = img;
