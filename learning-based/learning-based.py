efbb22980eb27543fd5eeeff980327413897ee7e
Noah Smith <noahhsmith@gmail.com>
2016-12-28 18:04:13 +0100

    sync

--- learning-based/learning-based.py
+++ learning-based/learning-based.py
@@ -1,24 +1,32 @@
-import numpy as np
+from optparse import OptionParser
 import mnist
 import tensorflow as tf
 import graph_nodes as gn
-FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('checkpoint_dir', '/home/noah/dev/train', 'model dir')
 
-images = mnist.read_images('/home/noah/dev/starid/data/images_b1.mnist')
-labels = mnist.read_labels('/home/noah/dev/starid/data/images_b2.mnist')
+def main():
+  usage = "usage: %prog [options] arg"
+  parser = OptionParser(usage)
+  parser.add_option('-s', '--starndx', dest='starndx', help='')
+  (options, args) = parser.parse_args()
 
-imgndx = 2
-label = labels[imgndx]
-image = images[imgndx,:,:,0]
-image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
-image = tf.reshape(image, [28, 28, 1])
+  FLAGS = tf.app.flags.FLAGS
+  tf.app.flags.DEFINE_string('checkpoint_dir', '/home/noah/dev/train', 'model dir')
+  images = mnist.read_images('/home/noah/dev/starid/data/images_b1.mnist')
+  labels = mnist.read_labels('/home/noah/dev/starid/data/images_b2.mnist')
+  imgndx = int(int(options.starndx) / 800) - 1
+  label = labels[imgndx] # label value in file is (starndx/800)-1, for starndxs 800, 1600, ... 8000
+  image = images[imgndx,:,:,0]
+  image = tf.cast(image,  if options.starndx:
+    print("starndx %s" % options.starndx)
+ tf.float32) * (1. / 255) - 0.5
+  image = tf.reshape(image, [28, 28, 1])
+  softmax = gn.inference(image)
+  saver = tf.train.Saver()
+  ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+  with tf.Session() as sess:
+    saver.restore(sess, ckpt.model_checkpoint_path)
+    softmaxval = sess.run(softmax)
+    print(softmaxval)
 
-softmax = gn.inference(image)
-saver = tf.train.Saver()
-ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
-with tf.Session() as sess:
-  saver.restore(sess, ckpt.model_checkpoint_path)
-  softmaxval = sess.run(softmax)
-  print(softmaxval)
-
+if __name__ == "__main__":
+  main()
--- rules-based/rules-based.cpp
+++ rules-based/rules-based.cpp
@@ -6,10 +6,10 @@
 enum  optionIndex { UNKNOWN, HELP, STARNDX };
 const option::Descriptor usage[] =
 {
-    {UNKNOWN, 0, "", "",option::Arg::None, "USAGE: example [options]\nOptions:" },
-    {HELP, 0, "", "help",option::Arg::None, "  --help  \tPrint usage and exit." },
-    {STARNDX, 0, "s", "starndx",option::Arg::Optional, "  --starndx, -s  \tStarndx." },
-    {0,0,0,0,0,0} // end of list marker
+    {UNKNOWN, 0, "", "",option::Arg::None, "usage: example [options]\noptions:" },
+    {HELP, 0, "", "help",option::Arg::None, "  --help  \tprint usage and exit." },
+    {STARNDX, 0, "s", "",option::Arg::Optional, "  -s[xxxx]  \tstarndx." },
+    {0,0,0,0,0,0} // end of options
 };
 
 int main(int argc, char* argv[])
@@ -24,27 +24,20 @@ int main(int argc, char* argv[])
         option::printUsage(std::cout, usage);
         return 0;
     }
-    if (options[STARNDX]) {
-        option::Option* opt1 = options[STARNDX];
-        std::cout << "Starndx " << opt1->arg << "\n";
-        return 0;
-    }
 
-    std::string fcatalog   = "/home/noah/dev/starid/data/SKYMAP_8876_stars.txt";
-    double mv              = 6.5; // sensor upper limit for visual magnitude
-    double fov             = 4.0 * arma::datum::pi / 180.0; // sensor field of view
-    uint starndx           = 800; // star to be used
-    double triangles_tol   = 60 * arma::datum::pi / 648e3; // tolerance for triangle variations
-    uint triangles_max     = 1e3; // upper limit for number of triangles
+    std::string fcatalog    = "/home/noah/dev/starid/data/skymap_8876.txt";
+    double mv               = 6.5; // sensor upper limit for visual magnitude
+    double fov              = 4.0 * arma::datum::pi / 180.0; // sensor field of view
+    double triangles_tol    = 60 * arma::datum::pi / 648e3; // tolerance for triangle variations
+    int triangles_max       = 1e3; // upper limit for number of triangles
+    int starndx             = 0;
+    if (options[STARNDX]) starndx = atoi(options[STARNDX].arg);
 
-    // create a sensor and make a star image
     stars::Sensor sensor(fcatalog, mv, fov);
     sensor.makeStarImage(starndx);
 
-    // create star triangles for the image and try to recognize a triangle
     triangles::TrianglesInStarImage triangles(sensor, triangles_tol, triangles_max);
     triangles.recognizeTriangleInStarImage();
-
     return 0;
 }
 
