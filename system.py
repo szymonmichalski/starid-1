dd31913778ef21b9ffab0889545f5175f9b0ab93
Noah Smith <noahhsmith@gmail.com>
2017-01-06 04:11:00 +0100

    sync

--- system.py
+++ system.py
@@ -7,12 +7,12 @@ starndx = 800
 
 t1 = time.time()
 starndx1 = lb.identifyCentralStar(starndx)
-print('lb %.3f ' % float(time.time() - t1))
+print('lb %.3f seconds' % float(time.time() - t1))
 
 t1 = time.time()
 out = subprocess.check_output(['/home/noah/dev/starid/rules-based/rb', '-s%d' % starndx])
 starndx2 = int(re.search(r'identification (\d+)', out.decode('utf-8')).group(1))
-print('rb %.3f ' % float(time.time() - t1))
+print('rb %.3f seconds' % float(time.time() - t1))
 
 print('true identity %d' % starndx)
 print('learning-based identification %d' % starndx1)
