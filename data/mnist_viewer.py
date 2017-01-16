4709440522b1adbc8bf81c09a2b523f326a88536
Noah Smith <noahhsmith@gmail.com>
2017-01-16 07:46:50 +0100

    mnist viewer

---
+++ data/mnist_viewer.py
@@ -0,0 +1,13 @@
+from data import mnist_to_tfrecords
+import numpy as np
+import matplotlib.pyplot as plt
+
+imgndx = 11
+
+images = mnist_to_tfrecords.read_images('/home/noah/dev/starid/data/images_b1.mnist')
+image = images[imgndx,:,:,0]
+# image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
+# image = tf.reshape(image, [28, 28, 1])
+
+plt.imshow(image)
+plt.show()
