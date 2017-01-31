262207f323902e5fe9a5ccd3910d0a67b8f6f8c5
Noah Smith <noahhsmith@gmail.com>
2017-01-31 21:42:01 +0100

    sync

---
+++ stars/data/mnist_viewer.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+import mnist_to_tfrecords
+
+imgndx = 13
+
+images = mnist_to_tfrecords.read_images('/home/noah/dev/starid/data/images_b1.mnist')
+image = -1 * images[imgndx,:,:,0]
+
+plt.matshow(image, cmap='Greys',  interpolation='nearest')
+plt.show()
