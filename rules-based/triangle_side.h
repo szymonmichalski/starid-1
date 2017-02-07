d3e8179cd037ff2e6ca548bab3422555ebd878d9
Noah Smith <noahhsmith@gmail.com>
2017-02-07 06:22:00 +0100

    sync

--- rules-based/triangle_side.h
+++ rules-based/triangle_side.h
@@ -1,6 +1,6 @@
 /// **triangle side**
 ///
-/// for adding, finding, removing, etc star pairs to a triangle side.
+/// for adding, finding, removing, etc star pairs to a triangle side. one objective here is to work with maps of unique star keys, one key per star, rather than maps with multiple keys per star. this means using unordered_map, not unordered_multimap.
 ///
 #ifndef TRIANGLE_SIDE_H
 #define TRIANGLE_SIDE_H
@@ -12,12 +12,12 @@ namespace rules {
 class TriangleSide {
 
 public:
-    TriangleSide();
+
     TriangleSide(double ang, double tol_radius, rules::PairsOverWholeSky& pairs);
 
 private:
 
-    /// **stars** each star is a map key, containing a map of twins, paired stars
+    /// **stars** each star is a map key, whose value is a map of star keys it pairs with
     ///
     using inner_map = std::unordered_map<int, int>;
     using outer_map = std::unordered_map<int, inner_map>;
