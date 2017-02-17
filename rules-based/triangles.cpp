8a80032d73f39ca039b9a21f23bdb7104449f192
Noah Smith <noahhsmith@gmail.com>
2017-02-18 00:19:56 +0100

    sync

--- rules-based/triangles.cpp
+++ rules-based/triangles.cpp
@@ -5,44 +5,38 @@ rules::Triangles::Triangles(stars::Image& image, rules::PairsOverWholeSky& pairs
 }
 
 int rules::Triangles::identifyCentralStar() {
-    using namespace Eigen;
     std::unordered_map<int, int> curstars;
-    int starsCnt = image.uvecs.n_rows;
-    int ndxi, ndxj, ndxk, dj, dk;
-
-    for (dj = 2; dj <= starsCnt-2; ++dj) {
-        for (dk = 2; dk <= starsCnt-dj-1; ++dk) {
-            for (ndxi = 2; ndxi <= starsCnt-dj-dk; ++ndxi) {
-                ndxj = ndxi + dj;
-                ndxk = ndxj + dk;
-
-                std::vector<double> angs;
-                arma::vec uveca = arma::trans(image.uvecs.row(0));
-                arma::vec uvecb = arma::trans(image.uvecs.row(ndxi-1));
-                arma::vec uvecc = arma::trans(image.uvecs.row(ndxj-1));
-                arma::vec uvecd = arma::trans(image.uvecs.row(ndxk-1));
-                angs.push_back(std::acos(arma::dot(uveca, uvecb)));
-                angs.push_back(std::acos(arma::dot(uvecb, uvecc)));
-                angs.push_back(std::acos(arma::dot(uvecc, uveca)));
-
-                bool skip = false;
-                for (int ndx1 = 0; ndx1 < 3; ++ndx1) {
-                    if (angs[ndx1] > stars::maxStarPairAngle)
+    int num_stars = image.uvecs.n_rows;
+    for (int ndxb = 1; ndxb < num_stars; ++ndxb) {
+        for (int ndxc = 1; ndxc < num_stars; ++ndxc) {
+            if (ndxc == ndxb) continue;
+            std::vector<double> angs;
+            arma::vec uveca = arma::trans(image.uvecs.row(0));
+            arma::vec uvecb = arma::trans(image.uvecs.row(ndxb));
+            arma::vec uvecc = arma::trans(image.uvecs.row(ndxc));
+            angs.push_back(std::acos(arma::dot(uveca, uvecb)));
+            angs.push_back(std::acos(arma::dot(uvecb, uvecc)));
+            angs.push_back(std::acos(arma::dot(uvecc, uveca)));
+            bool skip = false;
+            if (angs[0] < 4000.0 * M_PI / 648000.0) skip = true;
+            if (angs[2] < 4000.0 * M_PI / 648000.0) skip = true;
+            for (int ndx1 = 0; ndx1 < 3; ++ndx1) {
+                for (int ndx2 = ndx1; ndx2 < 3; ++ndx2) {
+                    if (ndx1 == ndx2) continue;
+                    if (std::abs(angs[ndx1]-angs[ndx2]) < 4000.0 * M_PI / 648000.0)
                         skip = true;
                 }
-                for (int ndx1 = 0; ndx1 < 3; ++ndx1) {
-                    for (int ndx2 = ndx1; ndx2 < 3; ++ndx2) {
-                        if (ndx1 == ndx2) continue;
-                        if (std::abs(angs[ndx1]-angs[ndx2]) < 3000.0 * M_PI / 648000.0)
-                            skip = true;
-                    }
-                }
-                if (skip) continue;
+            }
+            if (skip) continue;
+            Triangle abc(angs[0], angs[1], angs[2], tol_radius, pairsOverWholeSky);
 
-                Triangle abc(angs[0], angs[1], angs[2], tol_radius, pairsOverWholeSky);
+            for (int ndxd = 1; ndxd < num_stars; ++ndxd) {
+                if (ndxd == ndxb || ndxd == ndxc) continue;
 
-                std::unordered_map<int, int> merged; // = ad.stars_in_three_sides(ab, ac);
-                update_stars(curstars, merged);
+                arma::vec uvecd = arma::trans(image.uvecs.row(ndxd));
+
+//                std::unordered_map<int, int> merged; // = ad.stars_in_three_sides(ab, ac);
+//                update_stars(curstars, merged);
             }
         }
     }
