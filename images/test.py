93892ab8d6adde1fa89302ced2fbc88b001960fd
Noah Smith <noahhsmith@gmail.com>
2017-04-15 11:46:27 +0200

    Delete test.py

--- images/test.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-star_images_module = tf.load_op_library('libstar_images.so')
-with tf.Session(''):
-  out = star_images_module.star_images([[1, 2], [3, 4]]).eval()
-print(out)
