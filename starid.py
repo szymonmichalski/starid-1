049fcba7e6f78e1b0e9f6c3c314bede02a12f96a
Noah Smith <noahhsmith@gmail.com>
2017-04-24 14:52:45 +0200

    sync

--- starid.py
+++ starid.py
@@ -13,13 +13,12 @@ import lb.acontainer_make
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('checkpoint_dir', '/home/noah/starid/lb/adata', '')
 resultscnt = 5
-results = np.zeros(shape=(resultscnt, 5), dtype=float)
+results = np.zeros(shape=(resultscnt, 3), dtype=float)
 
 ### *lb a* uses a convolutional network with training and evaluation input files based on the classic mnist format. systems that process classic mnist are compatible.
 ###
 def lb_a(imgndx):
-    starsetndx = np.random.randint(0, 1000)
-    imgndx = starndx + starsetndx * 10
+    imgndx = starndx + 10 * np.random.randint(0, 1000)
     tf.reset_default_graph()
     images = lb.acontainer_make.read_images('/home/noah/starid/stars/data/eval_examples')
     image = images[imgndx, :, :, 0]
@@ -46,9 +45,5 @@ def rb_a(starndx):
 for resultsndx in range(0, resultscnt):
     starndx = np.mod(resultsndx, 10)
     results[resultsndx, 0] = starndx
-    t = time.time()
     results[resultsndx, 1] = lb_a(starndx)
-    results[resultsndx, 2] = float(time.time() - t)
-    t = time.time()
-    results[resultsndx, 3] = rb_a(starndx)
-    results[resultsndx, 4] = float(time.time() - t)
+    results[resultsndx, 2] = rb_a(starndx)
