0fc12ad399b2ee1cfd5fbeb338d9181f120e5050
noah <noahhsmith@gmail.com>
2017-04-30 12:40:17 +0200

    sync

--- identification/cnn2.py
+++ identification/cnn2.py
@@ -4,24 +4,39 @@
 import numpy as np
 import tensorflow as tf
 
-w = tf.Variable(tf.zeros([2, 1]))
-b = tf.Variable(0.)
+w1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1), dtype=tf.float32)
+b1 = tf.Variable(tf.constant(0.1, shape=[32]), dtype=tf.float32)
+w2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1), dtype=tf.float32)
+b2 = tf.Variable(tf.constant(0.1, shape=[64]), dtype=tf.float32)
+w3 = tf.Variable(tf.truncated_normal([7*7*64, 1024], stddev=0.1), dtype=tf.float32)
+b3 = tf.Variable(tf.constant(0.1, shape=[1024]), dtype=tf.float32)
+w4 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1), dtype=tf.float32)
+b4 = tf.Variable(tf.constant(0.1, shape=[10]), dtype=tf.float32)
 
 def inference(x):
-    ypred = tf.matmul(x, w) + b
-    return ypred
+    conv1 = tf.nn.conv2d(x, w1, strides=[1, 1, 1, 1], padding='SAME') + b1
+    acti1 = tf.nn.relu(conv1)
+    pool1 = tf.nn.max_pool(acti1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    conv2 = tf.nn.conv2d(pool1, w2, strides=[1, 1, 1, 1], padding='SAME') + b2
+    acti2 = tf.nn.relu(conv2)
+    pool2 = tf.nn.max_pool(acti2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    full3 = tf.matmul(tf.reshape(pool2, [-1, 7*7*64]), w3) + b3
+    acti3 = tf.nn.relu(full3)
+    drop3 = tf.nn.dropout(acti3, 1.0)
+    full4 = tf.matmul(drop3, w4) + b4
+    return tf.nn.softmax(full4)
 
-def loss(x, y):
-    lossval = 0
-    return lossval
+def loss(ypred, y):
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=ypred, labels=y)
+    return tf.reduce_mean(cross_entropy)
 
 def inputs():
-    x = np.array([0, 0])
-    y = 0
+    x = np.zeros((20, 28, 28, 1), dtype=np.float32)
+    y = np.zeros((20), dtype=np.int32)
     return x, y
 
 def train(lossval):
-    return
+    return tf.train.AdamOptimizer(1e-4).minimize(lossval)
 
 def evaluate(sess, x, y):
     return
@@ -30,7 +45,8 @@ saver = tf.train.Saver()
 with tf.Session() as sess:
     tf.global_variables_initializer().run()
     x, y = inputs()
-    lossval = loss(x, y)
+    ypred = inference(x)
+    lossval = loss(ypred, y)
     trainop = train(lossval)
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
