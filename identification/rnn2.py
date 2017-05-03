5c8e39512887a0d9e6893410a8fb68857e8e5fe7
Noah Smith <noahhsmith@gmail.com>
2017-05-03 07:30:25 +0200

    sync

--- identification/rnn2.py
+++ identification/rnn2.py
@@ -4,48 +4,39 @@
 import numpy as np
 import tensorflow as tf
 
-w = tf.Variable(tf.zeros([2, 1]))
-b = tf.Variable(0.)
 
-def inference(x):
-    ypred = tf.matmul(x, w) + b
-    return ypred
+data = tf.placeholder(tf.float32, [None, 36,1]) # number of sequences, length of sequences, dimension of each sequence step
+target = tf.placeholder(tf.float32, [None, 1])
 
-def loss(x, y):
-    lossval = 0
-    return lossval
+num_hidden = 24
+cell = tf.nn.rnn_cell.LSTMCell(num_hidden,state_is_tuple=True)
+val, _ = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
+val = tf.transpose(val, [1, 0, 2])
+last = tf.gather(val, int(val.get_shape()[0]) - 1)
+weight = tf.Variable(tf.truncated_normal([num_hidden, int(target.get_shape()[1])]))
+bias = tf.Variable(tf.constant(0.1, shape=[target.get_shape()[1]]))
+prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
+cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
+optimizer = tf.train.AdamOptimizer()
+minimize = optimizer.minimize(cross_entropy)
+mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(prediction, 1))
+error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
-def inputs():
-    x = np.array([0, 0])
-    y = 0
-    return x, y
+init_op = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init_op)
 
-def train(lossval):
-    return
-
-def evaluate(sess, x, y):
-    return
-
-saver = tf.train.Saver()
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    x, y = inputs()
-    lossval = loss(x, y)
-    trainop = train(lossval)
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-
-    training_steps = 1000
-    for step in range(training_steps):
-        # sess.run([trainop])
-        if step % 100 == 0:
-            print('loss ')
-            # print('loss ', sess.run([lossval]))
-        if step % 500 == 0:
-            saver.save(sess, 'data_rnn2/model', global_step=step)
-
-    evaluate(sess, x, y)
-    saver.save(sess, 'data_rnn2/model', global_step=training_steps)
-    coord.request_stop()
-    coord.join(threads)
-    sess.close()
+batch_size = 1000
+no_of_batches = int(len(train_input)) / batch_size
+epoch = 5000
+for i in range(epoch):
+    ptr = 0
+    for j in range(no_of_batches):
+        inp, out = train_input[ptr:ptr+batch_size], train_output[ptr:ptr+batch_size]
+        ptr+=batch_size
+        sess.run(minimize,{data: inp, target: out})
+    print "Epoch ",str(i)
+incorrect = sess.run(error,{data: test_input, target: test_output})
+print sess.run(prediction,{data: [[[1],[0],[0],[1],[1],[0],[1],[1],[1],[0],[1],[0],[0],[1],[1],[0],[1],[1],[1],[0]]]})
+print('Epoch {:2d} error {:3.1f}%'.format(i + 1, 100 * incorrect))
+sess.close()
