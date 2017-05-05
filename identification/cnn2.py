325268a1ad7ec4221bec6d3ca72a22bb31c33291
noah <noahhsmith@gmail.com>
2017-05-06 00:12:24 +0200

    sync

--- identification/cnn2.py
+++ identification/cnn2.py
@@ -5,8 +5,16 @@ import random
 import numpy as np
 import tensorflow as tf
 import libstarid.libstarid as ls
-
 libstarid = ls.libstarid()
+
+def inputs(batch_size):
+    images = np.zeros((batch_size, 28, 28, 1), dtype=np.float32)
+    labels = np.zeros((batch_size), dtype=np.int32)
+    for cnt in range(batch_size):
+        starndx = random.randint(0, 9)
+        images[cnt, :, :, 0] = libstarid.image(starndx=starndx)
+        labels[cnt] = starndx
+    return images, labels
 
 data = tf.placeholder(tf.float32, [None, 28, 28, 1])
 target = tf.placeholder(tf.int32, [None])
@@ -18,46 +26,29 @@ w3 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1), dtype=tf.f
 b3 = tf.Variable(tf.constant(0.1, shape=[1024]), dtype=tf.float32)
 w4 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1), dtype=tf.float32)
 b4 = tf.Variable(tf.constant(0.1, shape=[10]), dtype=tf.float32)
+conv1 = tf.nn.conv2d(data, w1, strides=[1, 1, 1, 1], padding='SAME') + b1
+pool1 = tf.nn.max_pool(tf.nn.relu(conv1), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+conv2 = tf.nn.conv2d(pool1, w2, strides=[1, 1, 1, 1], padding='SAME') + b2
+pool2 = tf.nn.max_pool(tf.nn.relu(conv2), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+full3 = tf.matmul(tf.reshape(pool2, [-1, 7 * 7 * 64]), w3) + b3
+drop3 = tf.nn.dropout(tf.nn.relu(full3), 1.0)
+logits = tf.matmul(drop3, w4) + b4
 
-def inputs(batch_size):
-    images = np.zeros((batch_size, 28, 28, 1), dtype=np.float32)
-    labels = np.zeros((batch_size), dtype=np.int32)
-    for cnt in range(batch_size):
-        starndx = random.randint(0, 9)
-        images[cnt, :, :, 0] = libstarid.image(starndx=starndx)
-        labels[cnt] = starndx
-    return images, labels
-
-def infer(data):
-    conv1 = tf.nn.conv2d(data, w1, strides=[1, 1, 1, 1], padding='SAME') + b1
-    pool1 = tf.nn.max_pool(tf.nn.relu(conv1), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    conv2 = tf.nn.conv2d(pool1, w2, strides=[1, 1, 1, 1], padding='SAME') + b2
-    pool2 = tf.nn.max_pool(tf.nn.relu(conv2), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    full3 = tf.matmul(tf.reshape(pool2, [-1, 7 * 7 * 64]), w3) + b3
-    drop3 = tf.nn.dropout(tf.nn.relu(full3), 1.0)
-    return tf.matmul(drop3, w4) + b4
-
-def evaluate(batch_size):
-    images, labels = inputs(batch_size=batch_size)
-    predicted = tf.cast(tf.arg_max(infer(images), 1), tf.int32)
-    return tf.reduce_mean(tf.cast(tf.equal(predicted, labels), tf.float32))
-
-logits = infer(data)
 cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=target))
 optimizer = tf.train.AdamOptimizer()
 minimize = optimizer.minimize(cross_entropy)
-accuracy = evaluate(batch_size=100)
+pred = tf.cast(tf.arg_max((logits), 1), tf.int32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(pred, target), tf.float32))
 
 saver = tf.train.Saver()
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 for batchndx in range(200):
     images, labels = inputs(batch_size=100)
-    sess.run([minimize], feed_dict={data: images, target: labels})
+    sess.run(minimize, {data: images, target: labels})
     if batchndx % 10 == 0:
-        print('batchndx %d loss %3.2f accuracy %3.2f'
-              % (batchndx, sess.run(cross_entropy, feed_dict={data: images, target: labels}),
-                 sess.run(accuracy, feed_dict={data: images, target: labels})))
-saver.save(sess, 'data_cnn2/model', global_step=batchndx)
+        images, labels = inputs(batch_size=100)
+        print('%5d %5.2f %5.2f' % (batchndx, sess.run(cross_entropy, {data: images, target: labels}), sess.run(accuracy, {data: images, target: labels})))
+# saver.save(sess, 'data_cnn2/model', global_step=batchndx)
 sess.close()
--- identification/rnn2.py
+++ identification/rnn2.py
@@ -7,9 +7,6 @@ import tensorflow as tf
 import libstarid.libstarid as ls
 libstarid = ls.libstarid()
 
-data = tf.placeholder(tf.float32, [None, 36,1])
-target = tf.placeholder(tf.float32, [None, 10])
-
 def inputs(batch_size):
     angseqs = np.zeros((batch_size, 36, 1), dtype=np.float32)
     starndxs = np.zeros((batch_size, 10), dtype=np.int32)
@@ -19,6 +16,8 @@ def inputs(batch_size):
         starndxs[batchndx, starndx] = 1
     return angseqs, starndxs
 
+data = tf.placeholder(tf.float32, [None, 36,1])
+target = tf.placeholder(tf.float32, [None, 10])
 num_hidden = 24
 cell = tf.contrib.rnn.BasicLSTMCell(num_hidden,state_is_tuple=True)
 val, _ = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
@@ -26,24 +25,23 @@ val = tf.transpose(val, [1, 0, 2])
 last = tf.gather(val, int(val.get_shape()[0]) - 1)
 weight = tf.Variable(tf.truncated_normal([num_hidden, int(target.get_shape()[1])]))
 bias = tf.Variable(tf.constant(0.1, shape=[target.get_shape()[1]]))
-prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
+softmax = tf.nn.softmax(tf.matmul(last, weight) + bias)
 
-cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction, 1e-10, 1.0)))
+cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(softmax, 1e-10, 1.0)))
 optimizer = tf.train.AdamOptimizer()
 minimize = optimizer.minimize(cross_entropy)
-mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(prediction, 1))
+mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(softmax, 1))
 accuracy = 1 - tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
 saver = tf.train.Saver()
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-for batchndx in range(500):
-    angseqs, starndxs = inputs(batch_size=100)
-    sess.run(minimize, {data: angseqs, target: starndxs})
+for batchndx in range(200):
+    images, labels = inputs(batch_size=100)
+    sess.run(minimize, {data: images, target: labels})
     if batchndx % 10 == 0:
-        print('batchndx %d loss %3.2f accuracy %3.2f'
-              % (batchndx, sess.run(cross_entropy, feed_dict={data: angseqs, target: starndxs}),
-                 sess.run(accuracy, feed_dict={data: angseqs, target: starndxs})))
-saver.save(sess, 'data_rnn2/model', global_step=batchndx)
+        images, labels = inputs(batch_size=100)
+        print('%5d %5.2f %5.2f' % (batchndx, sess.run(cross_entropy, {data: images, target: labels}), sess.run(accuracy, {data: images, target: labels})))
+# saver.save(sess, 'data_rnn2/model', global_step=batchndx)
 sess.close()
