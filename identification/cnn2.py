23685aaeb014a9854b371c15cb4c99bcc57aecd6
noah <noahhsmith@gmail.com>
2017-05-07 12:41:23 +0200

    sync

--- identification/cnn2.py
+++ identification/cnn2.py
@@ -28,16 +28,18 @@ w3 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1), dtype=tf.f
 b3 = tf.Variable(tf.constant(0.1, shape=[1024]), dtype=tf.float32)
 w4 = tf.Variable(tf.truncated_normal([1024, stars], stddev=0.1), dtype=tf.float32)
 b4 = tf.Variable(tf.constant(0.1, shape=[stars]), dtype=tf.float32)
+
 conv1 = tf.nn.conv2d(data, w1, strides=[1, 1, 1, 1], padding='SAME') + b1
 pool1 = tf.nn.max_pool(tf.nn.relu(conv1), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 conv2 = tf.nn.conv2d(pool1, w2, strides=[1, 1, 1, 1], padding='SAME') + b2
 pool2 = tf.nn.max_pool(tf.nn.relu(conv2), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 full3 = tf.matmul(tf.reshape(pool2, [-1, 7 * 7 * 64]), w3) + b3
 drop3 = tf.nn.dropout(tf.nn.relu(full3), 1.0)
-logits = tf.matmul(drop3, w4) + b4
-cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=target))
+output = tf.matmul(drop3, w4) + b4
+
+cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(output, target))
 minimize = tf.train.AdamOptimizer().minimize(cross_entropy)
-prediction = tf.cast(tf.arg_max((logits), 1), tf.int32)
+prediction = tf.cast(tf.arg_max((output), 1), tf.int32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, target), tf.float32))
 
 saver = tf.train.Saver()
--- identification/rnn2.py
+++ identification/rnn2.py
@@ -8,8 +8,8 @@ import libstarid.libstarid as ls
 libstarid = ls.libstarid()
 stars = 100
 batch = 100
-batches = 500
-hidden = 24
+batches = 1000
+lstmsize = 24
 
 def inputs(batch, stars):
     angseqs = np.zeros((batch, 36, 1), dtype=np.float32)
@@ -22,17 +22,19 @@ def inputs(batch, stars):
 
 data = tf.placeholder(tf.float32, [batch, 36,1])
 target = tf.placeholder(tf.float32, [batch, stars])
-cell = tf.contrib.rnn.BasicLSTMCell(hidden, state_is_tuple=True)
-out, state = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
-out = tf.transpose(out, [1, 0, 2])
-outf = tf.gather(out, int(out.get_shape()[0]-1))
-weight = tf.Variable(tf.truncated_normal([hidden, stars]))
-bias = tf.Variable(tf.constant(0.1, shape=[stars]))
-softmax = tf.nn.softmax(tf.matmul(outf, weight) + bias)
-cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(softmax, 1e-10, 1.0)))
+w1 = tf.Variable(tf.truncated_normal([lstmsize, stars]))
+b1 = tf.Variable(tf.constant(0.1, shape=[stars]))
+
+lstm = tf.contrib.rnn.BasicLSTMCell(lstmsize, state_is_tuple=True)
+outj, state = tf.nn.dynamic_rnn(lstm, data, dtype=tf.float32)
+outj = tf.transpose(outj, [1, 0, 2])
+outf = tf.gather(outj, int(outj.get_shape()[0]-1))
+output = tf.nn.softmax(tf.matmul(outf, w1) + b1)
+
+cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(output, 1e-10, 1.0)))
 minimize = tf.train.AdamOptimizer().minimize(cross_entropy)
-errvec = tf.not_equal(tf.argmax(target, 1), tf.argmax(softmax, 1))
-accuracy = 1 - tf.reduce_mean(tf.cast(errvec, tf.float32))
+prediction = tf.argmax((output), 1)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(target,1)), tf.float32))
 
 saver = tf.train.Saver()
 init = tf.global_variables_initializer()
