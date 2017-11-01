2c2697ca71bd8fc6f66a689869c1ad862e0d72c2
Noah Smith <noahhsmith@gmail.com>
2017-11-01 08:48:19 +0100

    sync

--- identification/recurrent.py
+++ identification/recurrent.py
@@ -1,4 +1,4 @@
-### *recurrent neural network* cnn using libstarid cpp eigen kernel and tensorflow training and inference
+### *recurrent neural network* rnn using libstarid cpp eigen kernel and tensorflow training and inference
 ###
 import random
 import numpy as np
