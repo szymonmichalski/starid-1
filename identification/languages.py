a6a837c5ef01b857be94fffc09e129b14db81272
Noah Smith <noahhsmith@gmail.com>
2017-11-02 08:55:54 +0100

    sync

--- identification/languages.py
+++ identification/languages.py
@@ -4,9 +4,12 @@ import languages_starimg
 import libstarid.libstarid as ls
 libstarid = ls.libstarid()
 
-image_info = libstarid.image_info(starndx=3)
-starimg = languages_starimg.languages_starimg(image_info)
+target = 3 # starndx of target star
 
-starimg.print_image_info()
-plt.matshow(-1 * starimg.image, cmap='Greys', interpolation='nearest')
-plt.show()
+info = libstarid.image_info(starndx=target)
+starimg = languages_starimg.languages_starimg(starndx=target, info=info)
+# starimg.print_image_info()
+# starimg.plot_image()
+
+starimg.starlist_prepare()
+starimg.starlist_print()
--- identification/languages_starimg.py
+++ identification/languages_starimg.py
@@ -1,14 +1,39 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 class languages_starimg:
 
-    def __init__(self, image_info):
-        self.image_info = image_info
+    def __init__(self, starndx, info):
+        self.starndx = starndx # starndx of target star
+        self.info = info
+        # use info to generate a 28 by 28 image pixel matrix
         self.image = np.zeros((28,28))
-        for rowndx in range(len(self.image_info)):
-            self.image[int(self.image_info[rowndx,0]), int(self.image_info[rowndx,1])] = 1.0
+        for rowndx in range(len(self.info)):
+            self.image[int(self.info[rowndx, 0]), int(self.info[rowndx, 1])] = 1.0
+        # starlist is for info ready for writing nouns, verbs, and sentences
+        self.starlist = []
 
     def print_image_info(self):
         np.set_printoptions(linewidth=200)
-        print(self.image_info)
+        print(self.info)
+
+    def plot_image(self):
+        plt.matshow(-1 * self.image, cmap='Greys', interpolation='nearest')
+        plt.show()
+
+    def starlist_prepare(self):
+        # pixel axj=row, pixel axi=col, starndx, radial distance
+        self.starlist.append([0, 0, int(self.starndx), 0])
+        for row in self.info:
+            x = row[1] - 13.5
+            y = 13.5 - row[0]
+            r = math.ceil(math.sqrt(x**2 + y**2))
+            self.starlist.append([int(row[0]), int(row[1]), int(row[2]), int(r)])
+        # sort by distance and starndx
+        self.starlist = sorted(self.starlist, key = lambda x: (x[3], x[2]))
+
+    def starlist_print(self):
+        import pprint
+        pprint.pprint(self.starlist)
 
