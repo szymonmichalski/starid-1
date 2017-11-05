7d851fb6c06ce7154f0afe60d8c376ddb27fe75e
Noah Smith <noahhsmith@gmail.com>
2017-11-05 08:43:47 +0100

    sync

--- identification/languages_starimg.py
+++ identification/languages_starimg.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import math
 
 class languages_starimg:
-
     def __init__(self, starndx, info):
         self.starndx = starndx # starndx of target star
         self.info = info
@@ -34,40 +33,39 @@ class languages_starimg:
 
 class lang1:
     def __init__(self, starlist):
-        self.noun1 = lang1_noun(starlist[0:3])
-        self.noun2 = lang1_noun(starlist[3:6])
+        self.noun1 = self.noun(starlist[0:3])
+        self.noun2 = self.noun(starlist[3:6])
         self.geom = self.noun1.geom + ', ' + self.noun2.geom
         self.ids = self.noun1.ids + ', ' + self.noun2.ids
 
-# each noun has two txt representations, geometric and starids
-class lang1_noun:
-    def __init__(self, stars):
-        self.stars = stars # star input for just this noun, three stars
-        id = [self.stars[0][0], self.stars[1][0], self.stars[2][0]]
-        x = [self.stars[0][3], self.stars[1][3], self.stars[2][3]]
-        y = [self.stars[0][4], self.stars[1][4], self.stars[2][4]]
-        side01 = math.sqrt((x[0] - x[1])**2 + (y[0] - y[1])**2)
-        side12 = math.sqrt((x[1] - x[2])**2 + (y[1] - y[2])**2)
-        side20 = math.sqrt((x[2] - x[0])**2 + (y[2] - y[0])**2)
-        sides = [
-            [0, 1, id[0], id[1], math.ceil(side01 / 4.), x[0], y[0], x[1], y[1]],
-            [1, 2, id[1], id[2], math.ceil(side12 / 4.), x[1], y[1], x[2], y[2]],
-            [2, 0, id[2], id[0], math.ceil(side20 / 4.), x[2], y[2], x[0], y[0]]]
-        sides = sorted(sides, key=lambda side: (side[4], side[0])) # increasing side length
-        sideab = str(sides[0][4])
-        sidebc = str(sides[1][4])
-        sideca = str(sides[2][4])
-        if (sides[0][0] == 0 and sides[1][0] == 1) \
-                or (sides[0][0] == 1 and sides[1][0] == 2) \
-                or (sides[0][0] == 2 and sides[1][0] == 0):
-            stara = str(sides[0][2])
-            starb = str(sides[0][3])
-            starc = str(sides[1][3])
-        else:
-            stara = str(sides[0][3])
-            starb = str(sides[0][2])
-            starc = str(sides[1][2])
-        self.sides = sides
-        self.geom = 'n:' + sideab + ':' + sidebc + ':' + sideca
-        self.ids = 'n:' + stara + ':' + starb + ':' + starc
+    class noun:
+        def __init__(self, stars):
+            self.stars = stars # star input for just this noun, three stars
+            id = [self.stars[0][0], self.stars[1][0], self.stars[2][0]]
+            x = [self.stars[0][3], self.stars[1][3], self.stars[2][3]]
+            y = [self.stars[0][4], self.stars[1][4], self.stars[2][4]]
+            side01 = math.sqrt((x[0] - x[1])**2 + (y[0] - y[1])**2)
+            side12 = math.sqrt((x[1] - x[2])**2 + (y[1] - y[2])**2)
+            side20 = math.sqrt((x[2] - x[0])**2 + (y[2] - y[0])**2)
+            sides = [
+                [0, 1, id[0], id[1], math.ceil(side01 / 4.), x[0], y[0], x[1], y[1]],
+                [1, 2, id[1], id[2], math.ceil(side12 / 4.), x[1], y[1], x[2], y[2]],
+                [2, 0, id[2], id[0], math.ceil(side20 / 4.), x[2], y[2], x[0], y[0]]]
+            sides = sorted(sides, key=lambda side: (side[4], side[0])) # increasing side length
+            sideab = str(sides[0][4])
+            sidebc = str(sides[1][4])
+            sideca = str(sides[2][4])
+            if (sides[0][0] == 0 and sides[1][0] == 1) \
+                    or (sides[0][0] == 1 and sides[1][0] == 2) \
+                    or (sides[0][0] == 2 and sides[1][0] == 0):
+                stara = str(sides[0][2])
+                starb = str(sides[0][3])
+                starc = str(sides[1][3])
+            else:
+                stara = str(sides[0][3])
+                starb = str(sides[0][2])
+                starc = str(sides[1][2])
+            self.sides = sides
+            self.geom = 'n:' + sideab + ':' + sidebc + ':' + sideca
+            self.ids = 'n:' + stara + ':' + starb + ':' + starc
 
